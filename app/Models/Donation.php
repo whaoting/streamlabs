e3b5a31a7b2f19c2910bed6fda86ee4f8a050ed9
Wei_Mac <whaoting@gmail.com>
2023-09-01 23:21:45 +0800

    feat: create table models
    include: users, followers, subscribers, donations, merch_sales

---
+++ app/Models/Donation.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use DateTime;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Donation extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, int, int, int, string, string, boolean, datetime, datetime>
+     */
+    protected $fillable = [
+        'id',
+        'user_id',
+        'target_user_id',
+        'amount',
+        'currency',
+        'message',
+        'is_read',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<int, int, int, int, string, string, boolean, datetime, datetime>
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'user_id' => 'integer',
+        'target_user_id' => 'integer',
+        'amount' => 'integer',
+        'currency' => 'string',
+        'message' => 'string',
+        'is_read' => 'boolean',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+}
---
+++ app/Models/User.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use DateTime;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string, string, datetime, datetime>
+     */
+    protected $fillable = [
+        'id',
+        'name',
+        'email',
+        'created_at',
+        'updated_at',
+//        'password',
+    ];
+
+//    /**
+//     * The attributes that should be hidden for serialization.
+//     *
+//     * @var array<int, string>
+//     */
+//    protected $hidden = [
+//        'password',
+//        'remember_token',
+//    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<int, string, string, datetime, datetime>
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'name' => 'string',
+        'email' => 'string',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+//        'password' => 'hashed',
+    ];
+}
